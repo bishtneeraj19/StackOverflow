9f2de841f23b536890ff0bdff2b65283fb21f0f3
NEERAJ BISHT <bishtneeraj243@gmail.com>
2021-07-19 16:24:03 +0530

    creating Activity

---
+++ app/src/main/java/com/creater/stackoverflow/AnswerActivity.kt
@@ -0,0 +1,11 @@
+package com.creater.stackoverflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnswerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
